b64d917abcfcbdc55c1ebf094edaf1dd92784c82
Andrew Hare <andrew@may17.org>
2016-01-21 15:22:05 -0700

    v0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="swimlane",
     author="Swimlane LLC",
     author_email="info@swimlane.com",
-    version="0.0.2",
+    version="0.0.3",
     url="https://github.com/Swimlane/sw-python-client",
     packages=find_packages(),
     description="A Python client for Swimlane.",
