4fc11decf228937ed969f53b8bfa2e5e7340a9ce
Austin <amcdaniel2@gmail.com>
2016-02-02 15:28:23 -0500

    Update test_core_auth.py

--- tests/test_core_auth.py
+++ tests/test_core_auth.py
@@ -8,12 +8,12 @@ def test_default_client():
         App.all()
 
 def test_user_pass_auth():
-    client = auth.Client("http://windows.local/swimlane/", "admin", "admin123")
+    client = auth.Client("server", "user", "password")
     client.connect()
     assert len(client.headers) == 1
 
 
 def test_user_pass_auth_wrong_creds():
     with pytest.raises(requests.HTTPError):
-        client = auth.Client("http://windows.local/swimlane/", "a", "b")
+        client = auth.Client("server", "user", "password")
         client.connect()
