793abe214b45f8ee2073f880160cbda8863870a1
Nick Allen <nick.allen@swimlane.com>
2017-04-07 15:51:01 -0600

    Add support for Swimlane 400 error codes

--- swimlane/core/client.py
+++ swimlane/core/client.py
@@ -1,12 +1,14 @@
 import weakref
 
 import requests
+from requests import HTTPError
 from six.moves.urllib.parse import urljoin
 from pyuri import URI
 
 from swimlane.core.resources.app import AppAdapter
 from swimlane.core.resources.group import GroupAdapter
 from swimlane.core.resources.user import UserAdapter
+from swimlane.errors import SwimlaneError
 
 
 class Swimlane(object):
@@ -19,9 +21,9 @@ class Swimlane(object):
         self.host.scheme = self.host.scheme or 'https'
         self.host.path = None
 
-        self.session = requests.Session()
-        self.session.verify = verify_ssl
-        self.session.auth = SwimlaneAuth(self, username, password)
+        self._session = requests.Session()
+        self._session.verify = verify_ssl
+        self._session.auth = SwimlaneAuth(self, username, password)
 
         self.apps = AppAdapter(self)
         self.users = UserAdapter(self)
@@ -34,12 +36,21 @@ class Swimlane(object):
         """Shortcut helper for sending requests to API endpoints
         
         Handles generating full API URL, session reuse and auth, and response status code checks
+        
+        Raises HTTPError on 4xx/5xx HTTP responses, or SwimlaneError on 400 responses with 
         """
         while endpoint.startswith('/'):
             endpoint = endpoint[1:]
 
-        response = self.session.request(method, urljoin(str(self.host) + self._api_root, endpoint), **kwargs)
-        response.raise_for_status()
+        response = self._session.request(method, urljoin(str(self.host) + self._api_root, endpoint), **kwargs)
+
+        try:
+            response.raise_for_status()
+        except HTTPError as e:
+            if e.response.status_code == 400:
+                raise SwimlaneError(e)
+            else:
+                raise
 
         return response
 
@@ -59,7 +70,7 @@ class Swimlane(object):
     def user(self):
         """Returns User record for authenticated user"""
         if not self.__user:
-            self.__user = self.users.get(username=self.session.auth.username)
+            self.__user = self.users.get(username=self._session.auth.username)
         return self.__user
 
 
---
+++ swimlane/errors.py
@@ -0,0 +1,71 @@
+"""Custom exceptions and errors"""
+
+# Swimlane custom error codes
+codes = {
+    -1: 'Unknown',
+    1000: 'PasswordExpired',
+    1001: 'DuplicateUserName',
+    1002: 'InvalidUserNameOrPassword',
+    1003: 'ConfirmPasswordDoesNotMatch',
+    1004: 'PasswordDoesNotMeetComplexityRequirements',
+    1005: 'PasswordResetRequired',
+    1006: 'NewPasswordCannotMatchCurrent',
+    1007: 'InvalidUser',
+    1051: 'DuplicateGroupName',
+    1061: 'DuplicateRoleName',
+    2000: 'DuplicateFieldName',
+    2001: 'FieldNameEmpty',
+    2002: 'InvalidApplicationExportFile',
+    2003: 'ApplicationNotFound',
+    2004: 'InvalidCalculation',
+    3000: 'DuplicateFieldValue',
+    3001: 'InvalidDateField',
+    3002: 'RecordNotFound',
+    3003: 'FieldNotFound',
+    4000: 'BadStatsGroup',
+    4001: 'BadFilter',
+    5000: 'AppLimitExceeded',
+    5001: 'UserLimitExceeded',
+    5002: 'NewServerInstall',
+    5003: 'UnableToConnectToActiveDirectory',
+    5004: 'UnableToRetrieveStoredValue',
+    5005: 'UnableToConnectToMongoDb',
+    5006: 'UnableToConnectToSmtp',
+    5007: 'SwimlaneAlreadyInitialized',
+    5008: 'ModelValidationError',
+    5009: 'UpgradeInProcess',
+    5010: 'RequiredFieldMissing',
+    6000: 'ConnectionDataNotProvided',
+    7000: 'RegexNotDefined',
+    7001: 'AssetNotFound',
+    9000: 'BadThreatIntelConnector',
+    9001: 'NoThreatIntel',
+    9002: 'ThreatIntelTypeNotSupportedByThisProvider',
+    10000: 'DuplicateTaskName',
+    10001: 'TaskNotFound'
+}
+
+
+class SwimlaneError(Exception):
+    """Base exception for Swimlane errors from 400 responses"""
+
+    def __init__(self, http_error):
+        self.http_error = http_error
+
+        try:
+            error_content = self.http_error.response.json()
+        except Exception:
+            error_content = {'Argument': None, 'ErrorCode': '-1'}
+
+        self.code = int(error_content['ErrorCode'])
+        self.argument = error_content['Argument']
+        self.name = codes.get(self.code, codes[-1])
+
+        if self.argument is None:
+            message = self.name
+        else:
+            message = '{name}({argument})'.format(name=self.name, argument=self.argument)
+
+        super(SwimlaneError, self).__init__(
+            '{message}: Bad Request for url: {url}'.format(message=message, url=self.http_error.response.url)
+        )
