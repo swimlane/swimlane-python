9d8d426c452492fb3d5e255d31f2c5f96f257b8d
Nick Allen <nick.allen@swimlane.com>
2017-05-25 16:07:23 -0600

    Remove Pypy from list of supported Python versions

--- setup.py
+++ setup.py
@@ -28,6 +28,5 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: Implementation :: PyPy"
     ]
 )
