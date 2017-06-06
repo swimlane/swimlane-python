a956a6bba08eb27aafb2dab115ca1e660dda521d
Nick Allen <nick.allen@swimlane.com>
2017-06-06 12:08:17 -0600

    Update README to rst for pypi

--- setup.py
+++ setup.py
@@ -5,13 +5,20 @@ def parse_requirements(requirement_file):
     with open(requirement_file) as f:
         return f.readlines()
 
+
+with open('./README.rst') as f:
+    long_description = f.read()
+
+
 setup(
     name="swimlane",
     author="Swimlane LLC",
     author_email="info@swimlane.com",
     url="https://github.com/swimlane/swimlane-python",
     packages=find_packages(exclude=('tests', 'tests.*')),
-    description="A Python client for Swimlane.",
+    description="Python driver for the Swimlane API",
+    long_description=long_description,
+    license='AGPLv3',
     install_requires=parse_requirements('./requirements.txt'),
     setup_requires=[
         'setuptools_scm',
