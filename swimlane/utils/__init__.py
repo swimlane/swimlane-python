43171fb8b978ba04593e9a30b6eee2edd2fc0832
Nick Allen <nick.allen@swimlane.com>
2017-10-27 15:50:42 -0600

    Fix for root package name collisions in dynamic recursive submodule imports

--- swimlane/utils/__init__.py
+++ swimlane/utils/__init__.py
@@ -34,8 +34,7 @@ def import_submodules(package):
 
     results = {}
 
-    for _, name, is_pkg in pkgutil.walk_packages(package.__path__):
-        full_name = package.__name__ + '.' + name
+    for _, full_name, is_pkg in pkgutil.walk_packages(package.__path__, package.__name__ + '.'):
         results[full_name] = importlib.import_module(full_name)
 
         if is_pkg:
