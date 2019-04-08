15c8797fcb7a5016bcb774c31342476fbf71893b
Michael Butler <michael.butler@swimlane.com>
2019-04-08 14:53:14 -0400

    Added history properties

--- swimlane/core/fields/history.py
+++ swimlane/core/fields/history.py
@@ -41,6 +41,8 @@ class Revision(APIResource):
 
     def __init__(self, record, raw):
         super(Revision, self).__init__(record._swimlane, raw)
+        self.__app_revision = None
+        self.__version = None
 
         self.record = record
 
@@ -48,25 +50,37 @@ class Revision(APIResource):
         self.revision_number = int(self._raw['revisionNumber'])
         self.status = self._raw['status']
 
-        #Get Correct App Revision for History Field
-        from swimlane.core.resources.app import App
-        self.app_revision_number = int(self._raw['version']['applicationRevision'])
-        self._app_revision_raw = self._swimlane.request(
-            'get',
-            '/app/{}/history/{}'.format(
-                self.record.app.id,
-                self.app_revision_number)).json()
-        self.app_revision = App(self._swimlane, self._app_revision_raw['version'])
-
         # UserGroupSelection, can't set as User without additional lookup
         self.user = UserGroup(self._swimlane, self._raw['userId'])
 
-        # Avoid circular imports
-        from swimlane.core.resources.record import Record
-        self.version = Record(self.app_revision, self._raw['version'])
-
     def __str__(self):
         return '{} ({})'.format(self.version, self.revision_number)
+
+    @property
+    def app_revision(self):
+        """Deferring request for app revision until needed"""
+        if not self.__app_revision:
+            # Avoid circular imports
+            from swimlane.core.resources.app import App
+
+            app_revision_number = int(self._raw['version']['applicationRevision'])
+            app_revision_raw = self._swimlane.request(
+                'get',
+                '/app/{}/history/{}'.format(
+                    self.record.app.id,
+                    app_revision_number)).json()
+            self.__app_revision = App(self._swimlane, app_revision_raw['version'])
+        return self.__app_revision
+
+    @property
+    def version(self):
+        """Deferring for sake of app revision"""
+        if not self.__version:
+            # Avoid circular imports
+            from swimlane.core.resources.record import Record
+
+            self.__version = Record(self.app_revision, self._raw['version'])
+        return self.__version
 
 
 class HistoryField(ReadOnly, CursorField):
