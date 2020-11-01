813410eb1f474b8627a1681b17ecd25e909039e0
Dmitriy Krasnikov <dmikov@gmail.com>
2020-11-01 15:40:12 -0500

    Not sure why didn't go

--- offline_installer/build.py
+++ offline_installer/build.py
@@ -1,111 +1,51 @@
-"""Creates dist/swimlane-python-offline-installer-<platform>-<py_version>.pyz offline driver installer archives"""
+"""Creates dist/swimlane-python-offline-installer-<platform>-<py_version>.pyz offline driver installer archives
+based on the platform it is run. Not to be used as a builder for different platform. For proper matrix builds."""
 import glob
 import os
 import tempfile
 import shutil
 import subprocess
 import platform
-from zipfile import ZipFile
 import sys
 
-# Map readable platform name to pip / .whl platform value
-PY_PLATFORMS = {'windows': 'win_amd64', 'linux': 'manylinux1_x86_64'}
+from zipfile import ZipFile
+from os import path
 
-# Python configs
+
 PY_VERSION = '{}{}'.format(sys.version_info.major, sys.version_info.minor)
-PY_PLATFORM = PY_PLATFORMS['windows' if platform.system() == 'Windows' else 'linux']
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+PY_PLATFORM = 'win_amd64' if platform.system() == 'Windows' else 'linux_x86_64'
+ROOT_DIR = path.dirname(path.dirname(path.abspath(__file__)))
 ALL_DEPS_DIR = tempfile.mkdtemp()
 CACHE_DIR = tempfile.mkdtemp()
 
-# Build local wheel
-print('Building swimlane wheel')
-os.chdir(os.path.join(ROOT_DIR, '..'))
+os.chdir(ROOT_DIR)
 cmd = 'pip --cache-dir="{cache}" wheel --no-deps -w "{deps_dir}" .'.format(deps_dir=ALL_DEPS_DIR, cache=CACHE_DIR)
-subprocess.check_call(cmd, shell=True)
-swimlane_whl = glob.glob(os.path.join(ALL_DEPS_DIR, 'swimlane-*'))[0]
+subprocess.check_call(cmd, stdout=subprocess.DEVNULL, shell=True)
+swimlane_whl = glob.glob(path.join(ALL_DEPS_DIR, 'swimlane-*'))[0]
 swimlane_version = swimlane_whl.split('/')[-1].split('-')[1]
-print('Built swimlane version ' + swimlane_version)
-
-# Collect and build all dependencies
-
-print('Downloading all dependencies to {}'.format(ALL_DEPS_DIR))
 
 os.chdir(ALL_DEPS_DIR)
+cmd = 'pip --cache-dir="{cache_dir}"  download -r {requirements} --prefer-binary'\
+    .format(cache_dir=CACHE_DIR, requirements=path.join(ROOT_DIR, 'requirements.txt'))
+subprocess.check_call(cmd, stdout=subprocess.DEVNULL, shell=True)
 
-# Initial pass to recursively grab all deps, regardless of platform support
-cmd = 'pip --cache-dir="{cache_dir}"  download -r {requirements}'\
-    .format(cache_dir=CACHE_DIR, requirements=os.path.join(ROOT_DIR, '..', 'requirements.txt'))
-subprocess.check_call(cmd, shell=True)
-
-ZIP_APP_DIR = tempfile.mkdtemp()
-REAL_DEPS_DIR = os.path.join(ZIP_APP_DIR, 'deps')
-os.mkdir(REAL_DEPS_DIR)
-os.chdir(REAL_DEPS_DIR)
-
-print('Collecting real dependencies to {}'.format(REAL_DEPS_DIR))
-
-for f in os.listdir(ALL_DEPS_DIR):
-
-    filename = os.path.join(ALL_DEPS_DIR, f)
-
-    if f.endswith('.tar.gz'):
-        f = f[:-7]
-
-    name, version = f.rsplit('-', 4)[:2]
-    package = '=='.join([name, version])
-
-    # Try to download platform-specific wheel
-    try:
-        abi_extension = 'mu' if PY_PLATFORM == 'manylinux1_x86_64' else 'm'
-        subprocess.check_call(
-            r'''
-            pip --cache-dir="{cache_dir}" download \
-            --no-deps \
-            --platform={platform} \
-            --python-version={version} \
-            --abi=cp{version}{abi_extension} \
-            --only-binary=:all: \
-            {package}
-            '''.format(
-                cache_dir=CACHE_DIR,
-                platform=PY_PLATFORM,
-                abi_extension=abi_extension,
-                version=PY_VERSION,
-                package=package
-            ),
-            shell=True
-        )
-    except Exception:
-        # Non-zero exit code, package not available as a platform-specific wheel, just copy in tar file as dep
-        shutil.copy(filename, REAL_DEPS_DIR)
-
-print('Collected dependencies to {}'.format(REAL_DEPS_DIR))
-
-# Create self-extracting zip installer
-
-print('Creating self-installing zip')
-DIST_DIR = os.path.join(ROOT_DIR, '..', 'dist')
-if not os.path.isdir(DIST_DIR):
+DIST_DIR = path.join(ROOT_DIR, 'dist')
+if not path.isdir(DIST_DIR):
     os.mkdir(DIST_DIR)
 
-zippath = os.path.realpath(os.path.join(
-    DIST_DIR,
-    'swimlane-python-{version}-offline-installer-{platform}-py{py_version}.pyz'.format(
-        version=swimlane_version,
-        platform=PY_PLATFORM,
-        py_version=PY_VERSION
-    )
-))
+zip_file_name = 'swimlane-python-{version}-offline-installer-{platform}-py{py_version}.pyz'\
+    .format(version=swimlane_version, platform=PY_PLATFORM, py_version=PY_VERSION)
+zip_path = path.realpath(path.join(DIST_DIR, zip_file_name))
 
-if os.path.exists(zippath):
-    os.remove(zippath)
+if os.path.exists(zip_path):
+    os.remove(zip_path)
 
-with ZipFile(zippath, 'w') as zipf:
-    zipf.write(os.path.join(ROOT_DIR, '__main__.py'), '__main__.py')
+with ZipFile(zip_path, 'w') as zip_file:
+    zip_file.write(path.join(ROOT_DIR, 'offline_installer', '__main__.py'), '__main__.py')
+    for f in os.listdir(ALL_DEPS_DIR):
+        zip_file.write(path.join(ALL_DEPS_DIR, f), path.join('deps', f))
 
-    for f in os.listdir(REAL_DEPS_DIR):
-        print('Adding file "{}"'.format(f))
-        zipf.write(os.path.join(REAL_DEPS_DIR, f), os.path.join('deps', f))
+print(zip_file_name)
 
-print('Offline installer written to {}'.format(zippath))
+shutil.rmtree(ALL_DEPS_DIR, ignore_errors=True)
+shutil.rmtree(CACHE_DIR, ignore_errors=True)
