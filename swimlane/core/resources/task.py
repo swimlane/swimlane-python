ce47b81119a09381a89675bbc5fe3618b85014ea
jmeachum <joshua.meachum@gmail.com>
2021-08-25 09:44:32 -0500

    Remove app property, not all tasks have an app.

--- swimlane/core/resources/task.py
+++ swimlane/core/resources/task.py
@@ -4,17 +4,12 @@ from swimlane.core.resources.base import APIResource
 class Task(APIResource):
     _type = 'Core.Models.Integrations.Task, Core'
 
-    def __init__(self, app, raw):
-        super(Task, self).__init__(app._swimlane, raw)
-
-        self.__app = app
+    def __init__(self, swimlane, raw):
+        super(Task, self).__init__(swimlane, raw)
+        self.app_id = raw.get('applicationId')
         self.id = raw.get('id')
         self.name = raw.get('name')
         self.script = raw.get('action').get('script')
-    
-    @property
-    def app(self):
-        return self.__app
     
     def __str__(self):
         return '{self.name}'.format(self=self)
--- tests/resources/test_task.py
+++ tests/resources/test_task.py
@@ -5,43 +5,17 @@ import pytest
 from swimlane.core.resources.app import App
 from swimlane.core.resources.task import Task
 
-
-@pytest.fixture(name='test_data', autouse=True, scope='module')
-def fixture_test_data(tmp_path_factory):
-    """
-    Copies all files from fixtures/apps and fixtures/tasks folders to tmp_dir.
-    """
-    temp_path = tmp_path_factory.mktemp('data', numbered=False)
-    app_files = Path(__file__).parent.joinpath('../fixtures/apps').glob('*')
-    tasks_files = Path(__file__).parent.joinpath('../fixtures/tasks').glob('*')
-    for file_name in app_files:
-        cp(str(file_name), str(temp_path))
-    for file_name in tasks_files:
-        cp(str(file_name), str(temp_path))
-    return temp_path
-
-
-@pytest.fixture(name='mock_task_app')
-def fixture_mock_task_app(test_data, mock_swimlane, request):
-    full_path = (test_data / request.param)
-    yield App(mock_swimlane, json.load(full_path.open()))
-
-
-@pytest.fixture(name='mock_task')
-def fixture_mock_task(test_data, mock_task_app, request):
-    full_path = (test_data / request.param)
-    yield Task(mock_task_app, json.load(full_path.open()))
-
-
-@pytest.mark.parametrize('mock_task_app, mock_task, task_filename', [
-    ('basic_app.json', 'sample_task.json', 'sample_task.json')
-    ], indirect=['mock_task_app', 'mock_task'] )
-def test_task_init(mock_task, mock_task_app, test_data, task_filename):
+@pytest.mark.parametrize('mock_task, task_filename', [
+    ('sample_task.json', 'sample_task.json'),
+    ('task_no_app.json', 'task_no_app.json')
+    ], indirect=['mock_task'], ids=['task_with_app', 'task_no_app']
+    )
+def test_task_init(mock_task, test_data, task_filename):
     task_json = json.load(test_data.joinpath(task_filename).open())
     task_name = task_json.get('name')
     assert mock_task.id == task_json.get('id')
     assert mock_task.name == task_name
-    assert mock_task.app == mock_task_app
+    assert mock_task.app_id == task_json.get('applicationId')
     assert mock_task.script == task_json.get('action').get('script')
     assert str(mock_task) == task_name
     assert mock_task.__repr__() == '<Task: {task_name}>'.format(task_name=task_name)
