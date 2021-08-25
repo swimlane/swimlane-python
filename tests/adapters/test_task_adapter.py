9286462fce4b175ccd9e9afd3c106403e748562f
jmeachum <joshua.meachum@gmail.com>
2021-08-25 11:05:56 -0500

    TaskAdapter and unit tests

---
+++ tests/adapters/test_task_adapter.py
@@ -0,0 +1,94 @@
+import pytest
+import mock
+from requests.models import Response
+from swimlane.core.adapters import TaskAdapter
+from swimlane.core.resources.task import Task
+
+
+@pytest.fixture(name='mock_task_adapter')
+def fixture_mock_task_adapter(mock_swimlane):
+    yield TaskAdapter(mock_swimlane)
+
+
+@pytest.mark.parametrize('mock_task', [
+    ('sample_task.json')
+    ], indirect=['mock_task'])
+def test_get(mock_task, mock_task_adapter):
+    # Requests mock needs to be installed
+    mock_return_list = [mock_task]
+    with mock.patch.object(mock_task_adapter, 'list', return_value=mock_return_list):
+
+        # Test by id
+        task = mock_task_adapter.get(id=mock_task.id)
+        assert task.id == mock_task.id
+
+        # Test by Name
+        task = mock_task_adapter.get(name=mock_task.name)
+        assert task.name == mock_task.name
+
+        # Test id not found raises ValueError
+        id_does_not_exist = 'id_does_not_exist'
+        with pytest.raises(ValueError) as exec_info:
+            mock_task_adapter.get(id=id_does_not_exist)
+        
+        assert str(exec_info.value) == str(ValueError('No task with id "{}"'.format(id_does_not_exist)))
+
+        # Test name not found raises ValueError
+        name_does_not_exist = 'name_does_not_exist'
+        with pytest.raises(ValueError) as exec_info:
+            mock_task_adapter.get(name=name_does_not_exist)
+        
+        assert str(exec_info.value) == str(ValueError('No task with name "{}"'.format(name_does_not_exist)))
+
+        invalid_key = 'invalid_key'
+        invalid_key_value = 'Invalid'
+        with pytest.raises(TypeError) as exec_info:
+            mock_task_adapter.get(invalid_key=invalid_key_value)
+        
+        assert str(exec_info.value) == str(TypeError('Unexpected arguments: {}'.format(
+            {invalid_key: invalid_key_value})))
+
+@pytest.mark.parametrize('mock_task', [
+     ('sample_task.json')
+     ], indirect=['mock_task'] )       
+def test_list(mock_task, mock_task_adapter):
+    mock_task_response = mock.Mock(spec=Response)
+    with mock.patch.object(mock_task_adapter._swimlane, 'request', return_value=mock_task_response):
+        mock_task_response.status_code = 200
+        mock_task_response.json.return_value = [mock_task._raw]
+        with mock.patch.object(mock_task_adapter, '_get_full', return_value=mock_task):
+            task_list = mock_task_adapter.list()
+
+            assert isinstance(task_list, list)
+            for task in task_list:
+                assert isinstance(task, Task)
+
+
+@pytest.mark.parametrize('mock_task', [
+     ('sample_task.json')
+     ], indirect=['mock_task'] )   
+def test_execute(mock_task_adapter, mock_task, mock_record):
+    with mock.patch.object(mock_task_adapter, 'get', return_value=mock_task):
+        mock_response = mock.Mock(spec=Response)
+        with mock.patch.object(mock_task_adapter._swimlane, 'request', return_value=mock_response) as mock_request:
+            response = mock_task_adapter.execute(mock_task.name, mock_record._raw)
+            assert response == mock_response
+            expected_json = {
+                'taskId': mock_task.id,
+                'record': mock_record._raw,
+            }
+            mock_request.assert_has_calls(calls=[mock.call('post', 'task/execute/record', json=expected_json)])
+            assert mock_request.call_count == 1
+
+
+@pytest.mark.parametrize('mock_task', [
+     ('sample_task.json')
+     ], indirect=['mock_task'] ) 
+def test_get_full(mock_task, mock_task_adapter):
+    mock_response = mock.Mock(spec=Response)
+    with mock.patch.object(mock_task_adapter._swimlane, 'request', return_value=mock_response):
+        mock_response.json.return_value = mock_task._raw
+        task_object = mock_task_adapter._get_full(mock_task.id)
+        assert isinstance(task_object, Task)
+        assert task_object == mock_task
+ 
