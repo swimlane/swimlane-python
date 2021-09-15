9387c53c547af672e510ac9f240215795b104068
jmeachum <joshua.meachum@gmail.com>
2021-09-14 21:47:36 -0500

    Test added for task name not found.

--- functional_tests/driver_tests/test_record_task_execution.py
+++ functional_tests/driver_tests/test_record_task_execution.py
@@ -14,7 +14,7 @@ def test_execute_task(app):
     # sample_task has input 'Single-line text field' and ouput 'Text List'.
     # Task splits input into a list and sends to output.
     record = app.records.create()
-    record["Single-line text field"] = "Testing 12345"
+    record['Single-line text field'] = 'Testing 12345'
     record.execute_task("sample_task")  
     assert record['Text List'][0] == 'Testing'
     assert record['Text List'][1] == '12345'
@@ -23,8 +23,11 @@ def test_execute_task(app):
 
 def test_execute_task_raises(app):
     record = app.records.create()
-    record["Single-line text field"] = "Testing 12345"
+    record['Single-line text field'] = 'Testing 12345'
     with pytest.raises(SwimlaneException):
         # Test that a failed task raises an exception.
-        record.execute_task("failed_task")
+        record.execute_task('failed_task')
+    with pytest.raises(ValueError):
+        # Test that a task name not found raises an exception.
+        record.execute_task('not_found')
     record.delete()
