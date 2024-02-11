4e59fee9cc407a2914fae996a81705d24a102227
Michel Oosterhof <michel@oosterhof.net>
2024-02-11 21:23:06 +0800

    PEP479

--- swimlane/core/resources/usergroup.py
+++ swimlane/core/resources/usergroup.py
@@ -156,8 +156,10 @@ class GroupUsersCursor(SwimlaneResolver, Cursor):
         if self._elements:
             for element in self._elements:
                 yield element
+            return
         else:
             for user_id in self.__user_ids:
                 element = self._swimlane.users.get(id=user_id)
                 self._elements.append(element)
                 yield element
+            return
